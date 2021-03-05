104f5749c612be88fa9d6a510fa7f2b0f17a65c0
Logan <Logan@kleditz.com>
2021-03-04 16:51:55 -0800

    Wrote test to check if the input is %100, meaning it isnt a leap year

--- test_leap_year.py
+++ test_leap_year.py
@@ -4,6 +4,7 @@ import leap_year
 class TestLeapYear(unittest.TestCase):
     def test_ly(self):
         self.assertEqual(leap_year.ly(4), True)
+        self.assertEqual(leap_year.ly(100), False)
         
 if __name__ == '__main__':
     
