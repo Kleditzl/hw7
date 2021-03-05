afd96a64aa71227bad9ab0edde361750a21d45db
Logan <Logan@kleditz.com>
2021-03-04 16:55:50 -0800

    Added test for % 400

--- test_leap_year.py
+++ test_leap_year.py
@@ -5,6 +5,7 @@ class TestLeapYear(unittest.TestCase):
     def test_ly(self):
         self.assertEqual(leap_year.ly(4), True)
         self.assertEqual(leap_year.ly(100), False)
+        self.assertEqual(leap_year.ly(400), True)
         
 if __name__ == '__main__':
     unittest.main()
