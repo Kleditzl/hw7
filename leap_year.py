f8234fd9dc3b1b68626ea0a5b842838e334fd1bf
Logan <Logan@kleditz.com>
2021-03-04 17:00:49 -0800

    Wrote the code to say if the number is % 400 then it is a leap year

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,11 @@
 def ly(a):
-    if a % 100 != 0:
-        if a % 4 == 0:
+    if a % 4 == 0:
+        if a % 100 == 0:
+            if a % 400 == 0:
+                return True
+            else:
+                return False
+        else:
             return True
     else:
         return False
