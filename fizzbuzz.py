14b518c672a6541dc6d0e9bdb3a47545bb3ab9df
Logan <Logan@kleditz.com>
2021-03-04 16:37:25 -0800

    Wrote the code, tests work

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,11 +3,13 @@ def fizz_print():
     for i in range(1, 101):
         if i % 3 == 0:
             a.append("fizz")
+        if i % 5 == 0:
+            a.append("buzz")
         else:
             a.append(i)
     lstToStr = ' '.join([str(elem) for elem in a])
-    
     return lstToStr
+
 
 if __name__ == "__main__":
     fizz_print()
